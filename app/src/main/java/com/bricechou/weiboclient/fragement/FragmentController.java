c63971c75fa7b4078c190c5d38935794f3771c8f
wangwan <wan.wang@T2Mobile.com>
2016-05-30 15:56:59 +0800

    update .gitignore and add main page

---
+++ app/src/main/java/com/bricechou/weiboclient/fragement/FragmentController.java
@@ -0,0 +1,69 @@
+package com.bricechou.weiboclient.fragement;//package com.bricechou.weiboclient.fragement;
+
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sdduser on 5/28/16.
+ */
+public class FragmentController {
+    //单例模式
+    //私有化构造方法
+    private int mcontainerId;//当前需要显示内容的ID
+    private FragmentManager mfragmentManager;
+    private ArrayList<Fragment> mfragments;
+    private FragmentTransaction mFragmentTransaction;
+
+    private static FragmentController scontroller;
+    public static FragmentController getInstance(FragmentActivity activity, int containerId) {
+        if(scontroller == null){
+            scontroller = new FragmentController(activity,containerId);
+        }
+        return scontroller;
+    }
+    private FragmentController(FragmentActivity activity,int containerId) {
+        this.mcontainerId = containerId;
+        mfragmentManager = activity.getSupportFragmentManager();
+        initFragment();
+    }
+
+    private void initFragment() {
+        mfragments = new ArrayList<Fragment>();
+        mfragments.add(new HomeFragment());
+        mfragments.add(new MessageFragment());
+        mfragments.add(new SearchFragment());
+        mfragments.add(new UserFragment());
+        mFragmentTransaction = mfragmentManager.beginTransaction();
+        for(Fragment fragment : mfragments) {
+            mFragmentTransaction.add(mcontainerId,fragment);
+        }
+        mFragmentTransaction.commit();
+    }
+
+    public void hideFragment() {
+        mFragmentTransaction = mfragmentManager.beginTransaction();
+        for(Fragment fragment : mfragments) {
+            if(fragment != null) {
+                mFragmentTransaction.hide(fragment);
+            }
+        }
+        mFragmentTransaction.commit();
+    }
+    public void showFragment(int position) {
+        hideFragment();
+        Fragment fragment = mfragments.get(position);
+        FragmentTransaction ft = mfragmentManager.beginTransaction();
+        ft.show(fragment);
+        ft.commit();
+    }
+
+    public Fragment getFragment(int position) {
+        return mfragments.get(position);
+    }
+}
---
+++ app/src/main/java/com/bricechou/weiboclient/fragement/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.bricechou.weiboclient.fragement;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bricechou.weiboclient.BaseFragment;
+import com.bricechou.weiboclient.R;
+
+/**
+ * Created by sdduser on 5/28/16.
+ */
+public class HomeFragment extends BaseFragment {
+    private View mView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = View.inflate(mMainActivity,R.layout.frag_home,null);
+        return mView;
+    }
+}
