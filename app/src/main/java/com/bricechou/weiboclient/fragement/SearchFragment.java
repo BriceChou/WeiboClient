9c4d8a542ce92b2e6589e7634f8bb0e92c824407
BriceChou <BriceChou@gmail.com>
2016-06-06 18:21:14 +0800

    add weibo content JSON data type

--- app/src/main/java/com/bricechou/weiboclient/fragement/SearchFragment.java
+++ app/src/main/java/com/bricechou/weiboclient/fragement/SearchFragment.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bricechou.weiboclient.BaseFragment;
+import com.bricechou.weiboclient.utils.BaseFragment;
 import com.bricechou.weiboclient.R;
 
 /**
