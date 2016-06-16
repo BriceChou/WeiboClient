c48a4d81c1b042a84be451a1118ec0fe03ea76e7
wangwan <wan.wang@T2mobile.com>
2016-06-16 10:13:10 +0800

    get friendlist data

--- app/src/main/java/com/bricechou/weiboclient/db/LoginUserToken.java
+++ app/src/main/java/com/bricechou/weiboclient/db/LoginUserToken.java
@@ -108,12 +108,12 @@ public class LoginUserToken {
      * @datetime 16-6-7 10:40
      * @XXX we don't input weibo account and password
      */
-    /*
+
     public static Oauth2AccessToken showAccessToken() {
         Oauth2AccessToken accessToken = new Oauth2AccessToken();
         accessToken.setToken("2.005KPAHDDkQmLCfb61244d50Rb_7xC");
         return accessToken;
     }
-    */
+
 
 }
