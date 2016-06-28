dcd81bdf832e3ab96c3a74a48db67519e408f338
BriceChou <BriceChou@gmail.com>
2016-06-28 18:18:53 +0800

    fixed detail page show BUG and add pullToRefresh

--- app/src/main/java/com/bricechou/weiboclient/fragment/HomeFragment.java
+++ app/src/main/java/com/bricechou/weiboclient/fragment/HomeFragment.java
@@ -14,27 +14,27 @@ import com.bricechou.weiboclient.config.Constants;
 import com.bricechou.weiboclient.db.LoginUserToken;
 import com.bricechou.weiboclient.utils.BaseFragment;
 import com.bricechou.weiboclient.utils.TitleBuilder;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.sina.weibo.sdk.openapi.StatusesAPI;
 import com.sina.weibo.sdk.openapi.models.StatusList;
 
 public class HomeFragment extends BaseFragment {
     private final static String TAG = "HomeFragment";
     private View mView;
-    private ListView mListViewHome; // fragment_home list view
+    private View mFootView;
+    private PullToRefreshListView mRefreshListViewHome; // fragment_home list view
     private StatusList mStatusList; // All Weibo content collection
     private StatusesAPI mStatusesAPI; // Weibo content interface
+    private HomeAdapter mHomeAdapter;
+    private int currentPage = 1;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        initView();
-        initWeiboContent();
-        /**
-         * Main page titlebar
-         *
-         * @comment by BriceChou
-         * @datetime 16-6-14 17:56
-         * @FIXME Show the current user name
-         */
+    private void initView() {
+        mView = View.inflate(mMainActivity, R.layout.frag_home, null);
+        mRefreshListViewHome = (PullToRefreshListView) mView.findViewById(R.id.lv_home);
+        // @comment by BriceChou
+        // @datetime 16-6-14 17:56
+        // @FIXME Show the current user name
         new TitleBuilder(mView)
                 // .setCenterText("" + mLoginUserInformation.loginUser.name)
                 .setCenterText("周晢")
@@ -43,6 +43,7 @@ public class HomeFragment extends BaseFragment {
                 .setLeftOnclickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
                     }
                 })
                 .setRightOnclickListener(new View.OnClickListener() {
@@ -50,6 +51,12 @@ public class HomeFragment extends BaseFragment {
                     public void onClick(View v) {
                     }
                 });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        initView();
+        initWeiboContent();
         return mView;
     }
 
@@ -65,7 +72,8 @@ public class HomeFragment extends BaseFragment {
                         // the Status instance load the data from JSON data.
                         mStatusList = mStatusList.parse(response);
                         if (null != mStatusList) {
-                            mListViewHome.setAdapter(new HomeAdapter(mMainActivity, mStatusList.statusList));
+                            mHomeAdapter = new HomeAdapter(mMainActivity, mStatusList.statusList);
+                            mRefreshListViewHome.setAdapter(mHomeAdapter);
                         } else {
                             onComplete(response);
                         }
@@ -75,8 +83,19 @@ public class HomeFragment extends BaseFragment {
         });
     }
 
-    private void initView() {
-        mView = View.inflate(mMainActivity, R.layout.frag_home, null);
-        mListViewHome = (ListView) mView.findViewById(R.id.lv_home);
+    private void refreshListViewHome(final int page) {
+        mRefreshListViewHome.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                refreshListViewHome(1);
+            }
+        });
+        mRefreshListViewHome.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+            @Override
+            public void onLastItemVisible() {
+                refreshListViewHome(currentPage + 1);
+            }
+        });
+        mFootView = View.inflate(mMainActivity, R.layout.footview_loading, null);
     }
 }
