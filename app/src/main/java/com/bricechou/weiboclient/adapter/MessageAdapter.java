3430590497d96fe27e5ab431ae3a29e912a202ec
BriceChou <BriceChou@gmail.com>
2016-07-06 18:27:06 +0800

    fixed message control-bar click toast information

--- app/src/main/java/com/bricechou/weiboclient/adapter/MessageAdapter.java
+++ app/src/main/java/com/bricechou/weiboclient/adapter/MessageAdapter.java
@@ -11,11 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bricechou.weiboclient.R;
-import com.bricechou.weiboclient.db.LoginUserToken;
 import com.bricechou.weiboclient.utils.TimeFormat;
 import com.bricechou.weiboclient.widget.CircleImageView;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.openapi.models.Comment;
 
 import java.util.ArrayList;
@@ -25,20 +23,17 @@ import java.util.ArrayList;
  *
  * @author BriceChou
  * @datetime 16-6-20 15:21
- * @TODO This is not important feature. So we finish it at the end of the project.
  */
 public class MessageAdapter extends BaseAdapter {
     private static final String TAG = "weiboclient.adapter.MessageAdapter";
     private Context mContext;
     private ArrayList<Comment> mComments;
     private ImageLoader mImageLoader;
-    private Oauth2AccessToken mOauthToken;
 
     public MessageAdapter(Context context, ArrayList<Comment> comments) {
         this.mContext = context;
         this.mComments = comments;
         mImageLoader = ImageLoader.getInstance();
-        mOauthToken = LoginUserToken.getAccessToken(context);
     }
 
     @Override
@@ -93,8 +88,8 @@ public class MessageAdapter extends BaseAdapter {
         mImageLoader.displayImage(comment.user.profile_image_url,
                 holder.sImageViewMessagePortrait);
         holder.sTextViewtvMessageUsername.setText(comment.user.name);
-        if (!TextUtils.isEmpty(comment.user.remark)){
-            holder.sTextViewtvMessageRemark.setText("("+comment.user.remark+")");
+        if (!TextUtils.isEmpty(comment.user.remark)) {
+            holder.sTextViewtvMessageRemark.setText("(" + comment.user.remark + ")");
         } else {
             holder.sTextViewtvMessageRemark.setText("");
         }
@@ -113,14 +108,14 @@ public class MessageAdapter extends BaseAdapter {
         holder.sLinearLayoutReply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext, "亲亲，您暂时没有权限查看哟!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "亲亲，您已经进行回复咯!", Toast.LENGTH_SHORT).show();
             }
         });
 
         holder.sLinearLayoutCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext, "亲亲，您已经进行回复咯!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "亲亲，您暂时没有权限查看哟!", Toast.LENGTH_SHORT).show();
             }
         });
         return convertView;
